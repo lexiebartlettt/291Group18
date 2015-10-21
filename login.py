1802b060128d2002ceec5496f909d7a6f32dd3d5
Bradley Lockhart <blockhar@cs.ualberta.ca>
2015-10-21 16:25:32 -0600

    Initial basic funtionality.

--- login.py
+++ login.py
@@ -11,15 +11,36 @@ try:
 	connection = cx_Oracle.connect(connStr)
 	curs = connection.cursor()
 
-except:
-	print("error")
-
-curs.execute('Drop table temp')
-connection.commit()
-
+except cx_Oracle.DatabaseError as exc:
+	error, = exc.args
+	print( sys.stderr, "Oracle code:", error.code)
+	print( sys.stderr, "oracle message:", error.message)
+	print("Connection Failed. Exiting Program")
+	sys.exit
 try:
-	curs.execute('Create table temp (var1 INTEGER)')
-	connection.commit()
+	print("Welcome to group 18's airline management system.")
+	print("Type Login, Register or Exit to continue.")
+
+	userInput = input()
+
+	while True:
+		if userInput.strip().lower() in ('login', 'log', 'l'):
+			print("Logging in.")
+			#userLogin()
+			break
+		elif userInput.strip().lower() in ('register', 'reg', 'r'):
+			print("Registering")
+			#registerUser()
+			break
+		elif userInput.strip().lower() in ('exit', 'e'):
+			print("Goodbye.")
+			sys.exit
+			break #reduanant but just in case...
+		else:
+			print("Input not recognised.")
+			print("Type Login, Register or Exit to continue.")
+			userInput = input()
+	
 	
 except cx_Oracle.DatabaseError as exc:
 	error, = exc.args
