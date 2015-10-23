36027db1a10e83ef32112d0fbd025acc667898d6
Bradley Lockhart <blockhar@ualberta.ca>
2015-10-23 11:02:53 -0600

    SQL query to create good_connections view

---
+++ goodConnections.sql
@@ -0,0 +1,6 @@
+reate view good_connections (src,dst,dep_date,flightno1,flightno2, layover,price) as
+  select a1.src, a2.dst, a1.dep_date, a1.flightno, a2.flightno, a2.dep_time-a1.arr_time,
+	min(a1.price+a2.price)
+  from available_flights a1, available_flights a2
+  where a1.dst=a2.src and a1.arr_time +1.5/24 <=a2.dep_time and a1.arr_time +5/24 >=a2.dep_time
+  group by a1.src, a2.dst, a1.dep_date, a1.flightno, a2.flightno, a2.dep_time, a1.arr_time
