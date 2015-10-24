0d90eefd2003937e67ac36855cfc785d339ddc57
Bradley Lockhart <blockhar@cs.ualberta.ca>
2015-10-23 18:29:22 -0600

    Updated to include information to allow for drill down

--- summaryBooking.sql
+++ summaryBooking.sql
@@ -1,4 +1,4 @@
-select  t.tno, t.name, to_char(b.dep_date, 'dd-mon-yy'), t.paid_price
+select  t.tno, t.name, to_char(b.dep_date, 'dd-mon-yy'), t.paid_price, b.flightno, b.dep_date
 from tickets t, bookings b
 where b.tno = t.tno
 and t.email = :email
