c0d52aff2b78b7d23f9070df2069c92ae215dfb7
fdrury <fdrury@ualberta.ca>
2015-10-26 14:55:35 -0600

    testing refund

--- cancelBooking.py
+++ cancelBooking.py
@@ -1,30 +1,26 @@
 import cx_Oracle # the package used for accessing Oracle in Python
 
-def cancelBooking(ticketNum, flightNum, depDate, curs):
+def cancelBooking(ticketNum, flightNum, depDate, fareType curs):
   try:
     curs.connection.begin()
     
-    '''
-    queryStr = "SELECT price FROM flight_fares WHERE(flightno = 'flightNum' and fare = fare)"
+    queryStr = "SELECT price FROM flight_fares WHERE(flightno = 'flightNum' and fare = 'fareType')"
     queryStr = queryStr.replace("flightNum", flightNum)
-    queryStr = queryStr.replace("depDate", depDate)
-    print(queryStr)
+    queryStr = queryStr.replace("fareType", fareType)
     curs.execute(queryStr)
     fetcher = curs.fetchall()
-    print (fetcher)
     refund = fetcher[0][0]
     
     queryStr = "SELECT paid_price FROM tickets WHERE tno = ticketNum"
-    queryStr = queryStr.replace("ticketNum", ticketNum)
+    queryStr = queryStr.replace("ticketNum", str(ticketNum))
     curs.execute(queryStr)
     fetcher = curs.fetchall()
     newPrice = fetcher[0][0] - refund
     
     queryStr = "UPDATE tickets SET price_paid = 'newPrice' WHERE tno = ticketNum"
-    queryStr = queryStr.replace("ticketNum", ticketNum)
+    queryStr = queryStr.replace("ticketNum", str(ticketNum))
     queryStr = queryStr.replace("newPrice", newPrice)
     curs.execute(queryStr)
-    '''
 
     queryStr = "DELETE FROM bookings WHERE(tno = ticketNum and flightno = 'flightNum' and dep_date = to_date('depDate', 'dd-mon-yy'))"
     queryStr = queryStr.replace("ticketNum", str(ticketNum))
