5161aaeaec4268626e31549bd531bee451d46501
Bradley Lockhart <blockhar@cs.ualberta.ca>
2015-10-26 12:44:08 -0600

    fixed to delete booking, does not refund

--- cancelBooking.py
+++ cancelBooking.py
@@ -1,14 +1,17 @@
 import cx_Oracle # the package used for accessing Oracle in Python
 
-def cancelBooking(ticktNum, flightNum, depDate, curs):
+def cancelBooking(ticketNum, flightNum, depDate, curs):
   try:
     curs.connection.begin()
     
-    queryStr = "SELECT price FROM flight_fares WHERE(flightno = 'flightNum' and fare = 'depDate')"
+    '''
+    queryStr = "SELECT price FROM flight_fares WHERE(flightno = 'flightNum' and fare = fare)"
     queryStr = queryStr.replace("flightNum", flightNum)
     queryStr = queryStr.replace("depDate", depDate)
+    print(queryStr)
     curs.execute(queryStr)
     fetcher = curs.fetchall()
+    print (fetcher)
     refund = fetcher[0][0]
     
     queryStr = "SELECT paid_price FROM tickets WHERE tno = ticketNum"
@@ -21,14 +24,17 @@ def cancelBooking(ticktNum, flightNum, depDate, curs):
     queryStr = queryStr.replace("ticketNum", ticketNum)
     queryStr = queryStr.replace("newPrice", newPrice)
     curs.execute(queryStr)
-    
-    queryStr = "DELETE FROM bookings WHERE(tno = ticketNum and flightno = 'flightNum')"
-    queryStr = queryStr.replace("ticketNum", ticketNum)
+    '''
+
+    queryStr = "DELETE FROM bookings WHERE(tno = ticketNum and flightno = 'flightNum' and dep_date = to_date('depDate', 'dd-mon-yy'))"
+    queryStr = queryStr.replace("ticketNum", str(ticketNum))
     queryStr = queryStr.replace("flightNum", flightNum)
+    queryStr = queryStr.replace("depDate", depDate)
     curs.execute(queryStr)
     
     curs.connection.commit()
     print("You have successfully cancelled your booking")
+    input("Press 'Enter' to return to main menu")
   
   except cx_Oracle.DatabaseError as exc:
     error, = exc.args
